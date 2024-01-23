9252cd88e4c07f81bb44ac384ecb50773bd5d810
niksy <niksy@DESKTOP-SFKOIVS>
2024-01-24 02:49:13 +0300

    Изменен код, теперь заместо "Hello world!!" будет "Дарова кенты!"

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace ovnlaba10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            Console.WriteLine("Дарова кенты!");
         }
     }
 }
